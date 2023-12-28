35c10a7efd79e9aab65a5f57ba7513c70dd1e1dd
arsenyoung <molodoiars@gmail.com>
2023-12-28 13:04:29 +0400

    feat: dynamic logic was added

---
+++ TODO_LIST/todo/script.js
@@ -0,0 +1,98 @@
+const buttonAddTask = document.getElementById('buttonCriar');
+const inputField = document.getElementById('inputTask');
+const taskListFormListEmpty = document.getElementById('taskListFormListEmpty');
+const taskListFormList = document.getElementById('taskListFormList');
+const taskItemPattern = document.getElementById('taskItemPattern');
+const totalTaskCounterItem = document.getElementById('totalTaskCounter');
+const completedTaskCounterItem = document.getElementById('completedTaskCounter');
+let tasks = [];
+let totalTaskCounter = 0
+let completedTaskCounter = 0;
+
+
+inputField.addEventListener('click', () => {
+    if (inputField.classList.contains('error')) {
+        inputField.classList.remove('error');
+        inputField.style.border = "1px solid #0D0D0D";
+    }
+})
+
+buttonAddTask.addEventListener('click', () => {
+    let taskDescription = inputField.value;
+
+    if (taskDescription) {
+        addTask(taskDescription);
+        changeFormListVisability();
+        updateCounters();
+    } else {
+        showError();
+    }
+})
+
+function addTask(taskDescription) {
+    let newTask = taskItemPattern.cloneNode(true);
+
+    inputField.value = '';
+    newTask.id = '';
+    newTask.children[1].innerText = taskDescription;
+    totalTaskCounter++;
+
+    if (totalTaskCounter == 1) {
+        taskListFormList.appendChild(newTask);
+    } else {
+        taskListFormList.insertBefore(newTask, taskListFormList.children[0]);
+    }
+}
+
+function updateCounters() {
+    totalTaskCounterItem.innerHTML = totalTaskCounter;
+    completedTaskCounterItem.innerHTML = completedTaskCounter + " / " + totalTaskCounter; 
+}
+
+function changeFormListVisability() {
+    if (totalTaskCounter > 0) {
+        taskListFormListEmpty.style.display = "none";
+        taskListFormList.style.display = "flex";
+    } else {
+        taskListFormListEmpty.style.display = "flex";
+        taskListFormList.style.display = "none";
+    }
+}
+
+function showError() {
+    if (!inputField.classList.contains('error')) {
+        inputField.classList.add('error');
+        inputField.style.border = "2px solid #E25858";
+    }
+}
+
+function deleteTask(event) {
+    let elemForDel = event.target.parentNode;
+
+    while (elemForDel.className != 'taskItem') {
+        elemForDel = elemForDel.parentNode;
+    } 
+
+    elemForDel.remove();
+    totalTaskCounter--;
+    updateCounters();
+}
+
+function changeCheckbox(event) {
+    let checkbox = event.target;
+    let taskItem = checkbox.parentNode;
+
+    if (checkbox.checked) {
+        completedTaskCounter++;
+        if (totalTaskCounter > 1) {
+            taskListFormList.appendChild(taskItem);
+        }
+    } else {
+        completedTaskCounter--;
+        if (totalTaskCounter > 1) {
+            taskListFormList.insertBefore(taskItem, taskListFormList.children[0]);
+        }
+    }
+
+    updateCounters();
+}
